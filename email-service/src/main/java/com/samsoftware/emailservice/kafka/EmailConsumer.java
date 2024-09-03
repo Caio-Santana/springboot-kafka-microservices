0378389e72e1c2a1f89d9c39c5a0cbb99d428ad8
Caio-Santana <caio.santana@live.com>
2024-09-02 21:11:33 -0300

    First commit

---
+++ email-service/src/main/java/com/samsoftware/emailservice/kafka/EmailConsumer.java
@@ -0,0 +1,23 @@
+package com.samsoftware.emailservice.kafka;
+
+import com.samsoftware.basedomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmailConsumer.class);
+
+    @KafkaListener(
+            topics = "${spring.kafka.topic.name}",
+            groupId = "${spring.kafka.consumer.group-id}"
+    )
+    public void consume(OrderEvent event) {
+        LOGGER.info("Order event received in email service -> {}", event);
+
+        // save the order event into the database
+    }
+}
---
+++ email-service/src/test/java/com/samsoftware/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsoftware.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
